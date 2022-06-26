37e2c7f27d78d95708b5be9806f842472e4bcce4
“fausto-gjs” <faustogarciajacinto@gmail.com>
2022-06-25 22:30:56 -0300

    botao hamburguer

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -4,7 +4,13 @@ export default function Header() {
 
     const btnMenu = () => {
         const bar = document.querySelector('.menu');
+        const menu1 = document.querySelector('.btnMobile1');
+        const menu2 = document.querySelector('.btnMobile2');
+        const menu3 = document.querySelector('.btnMobile3');
         bar.classList.toggle('active')
+        menu1.classList.toggle('active')
+        menu2.classList.toggle('active')
+        menu3.classList.toggle('active')
     }
 
     return (
@@ -13,13 +19,17 @@ export default function Header() {
                 <BsCodeSlash className="iconLogo" />
                 <h1>Fausto Garcia Jacinto Silva</h1>
             </div>
-        <div className="controls">
                 <div className="menu">
                     <Link to='/'>Sobre</Link>
                     <Link to='/portfolio'>Portfolio</Link>
                     <Link to='/contact'>Contato</Link>
                 </div>
-                <button className='btnMobile' onClick={() => btnMenu()}>Menu</button>
+        <div className="controls">
+                <div className="btnMobile"  onClick={() => btnMenu()} >
+                    <div className="btnMobile1"></div>
+                    <div className="btnMobile2"></div>
+                    <div className="btnMobile3"></div>
+                </div>
             </div>
         </div>
     )
