ac29379655726bc79df2dc7dc06d9a565c8b4d64
“fausto-gjs” <faustogarciajacinto@gmail.com>
2022-07-06 18:07:15 -0300

    connect backend

--- src/components/common/api/index.js
+++ src/components/common/api/index.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
 export default axios.create({
-    baseURL:`https://fausto-developer-api.herokuapp.com/`
+    baseURL:`https://fausto-developer-api.herokuapp.com/api/`
 });
--- src/components/pages/Portfolio/index.js
+++ src/components/pages/Portfolio/index.js
@@ -1,17 +1,25 @@
-import React,{useEffect} from 'react'
+import React,{useEffect,useState} from 'react'
 import {motion} from 'framer-motion'
 import animations2 from '../../effects/animations2'
 import Gif from '../../../assets/imgs/developer.gif'
 import api from '../../common/api'
 export default function Portfolio() {
   
+  const [PortfolioAPI,setPortfolioAPI]=useState([])
+
+  async function portfolio(){
+    const response = await api.get('portfolio')
+    setPortfolioAPI(response.data)
+    console.log(PortfolioAPI)
+  }
+  portfolio()
+  
   useEffect(()=>{
-    api.get('portfolio').then(res=>{
-      console.log(res)
-    })
+   
   },[])
 
     return (
+      <>
         <motion.div
         initial='init'
         animate='show'
@@ -21,6 +29,14 @@ export default function Portfolio() {
         >
           <img src={Gif} alt='developer'/>
           <h1>Em breve novos projetos</h1>
+
+         {PortfolioAPI.map(api=>(
+          <li key={api.id}>
+            <h2>{api.titulo}</h2>
+            <p>{api.description}</p>
+          </li>
+         ))}
         </motion.div>
+        </>
     )
 }
